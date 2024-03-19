6d5839e447273694620da686714fe28239bc6adc
Opoku Godfred <jupiter3x3cu7er@gmail.com>
2024-03-19 15:13:39 -0500

    first commit

---
+++ cht/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+function App() {
+	return (
+		<div className="container">
+      <ChatEngine 
+			projectID='ea5f8372-2073-4db3-8e4d-7c17bf09edc4'
+			userName='3x3cu7er'
+			userSecret='1111'
+		/>
+    </div>
+	);
+}
+
+export default App;
